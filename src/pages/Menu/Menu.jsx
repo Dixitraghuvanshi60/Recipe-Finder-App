941359537ae2fd06ea03b300dbe1dfe17df64a89
DixitRaghuvanshi60 <raghuvanshidixit60@gmail,com>
2025-11-13 10:31:31 +0530

    make own api in meals.json

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,33 +1,64 @@
-import { useEffect, useState } from 'react'
-import FoodBox from '../../components/FoodBox'
-import "./Menu.css"
+// import { useEffect, useState } from 'react'
+// import FoodBox from '../../components/FoodBox'
+// import "./Menu.css"
+
+// const Menu = () => {
+ 
+//   const [categories, setCategories] = useState([])
+//   const displayFood = async () => {
+//     const res = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+//     const data = await res.json()
+//     setCategories(data.categories)
+
+//   }
+//   useEffect(() => {
+//     displayFood()
+//   }, [])
+
+//   return (
+//     <section id='menu'>
+       
+//         <div id='categoryContainer'>
+        
+//         {
+//           categories.map((category)=>{
+//             return <FoodBox key={category.idCategory} category={category}/>
+//           })
+//         }
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default Menu
+
+
+
+import { useEffect, useState } from "react";
+import FoodBox from "../../components/FoodBox";
+import "./Menu.css";
+import data from "../../data/meals.json"; // ✅ Import your local JSON file
 
 const Menu = () => {
- 
-  const [categories, setCategories] = useState([])
-  const displayFood = async () => {
-    const res = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-    const data = await res.json()
-    setCategories(data.categories)
+  const [categories, setCategories] = useState([]);
 
-  }
   useEffect(() => {
-    displayFood()
-  }, [])
+    // ✅ Load local data instead of fetching from API
+    setCategories(data.categories);
+  }, []);
 
   return (
-    <section id='menu'>
-       
-        <div id='categoryContainer'>
-        
-        {
-          categories.map((category)=>{
-            return <FoodBox key={category.idCategory} category={category}/>
-          })
-        }
+    <section id="menu">
+      <div id="categoryContainer">
+        {categories.map((category) => (
+          <FoodBox
+            key={category.idCategory}
+            category={category}
+          />
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
